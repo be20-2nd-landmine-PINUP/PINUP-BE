7b93d00fbc0dc54483e0634d15cce9abb1fa9606
yeonsoo89 <dustn9689@gmail.com>
2025-10-29 16:37:40 +0900

    chore : member 도메인 구조 간단히 설계

---
+++ src/main/java/pinup/backend/member/entity/Member.java
@@ -0,0 +1,15 @@
+package pinup.backend.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+}
