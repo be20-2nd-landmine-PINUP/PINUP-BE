be559f272fd336a73bfd15276ac91e64da8f6c00
sangdo19909-code <sangdo19909@gmail.com>
2025-10-30 16:18:41 +0900

    hotfix : store기능 @EmbeddedId 추가했습니다.

--- src/main/java/pinup/backend/store/entity/Inventory.java
+++ src/main/java/pinup/backend/store/entity/Inventory.java
@@ -2,8 +2,10 @@ package pinup.backend.store.entity;
 
 import groovy.transform.builder.Builder;
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.catalina.Store;
@@ -13,8 +15,13 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Inventory {
+
+    @EmbeddedId
+    private InventoryKey id;
+
 
     @JoinColumn(name = "item_id", nullable = false)
     private Store store;
