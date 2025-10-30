2b1b5a1b28faf623b6c32f35459c6d61a3e4be56
sangdo19909-code <sangdo19909@gmail.com>
2025-10-30 18:21:49 +0900

    feature : store기능
    Enum부분 추가하였습니다.

--- src/main/java/pinup/backend/store/enums/StoreItemCategory.java
+++ src/main/java/pinup/backend/store/enums/StoreItemCategory.java
@@ -5,13 +5,15 @@ import lombok.Getter;
 @Getter
 public enum StoreItemCategory {
 
-    BACKGROUD("BG", "배경 아이템"),
+    BACKGROUND("BG", "배경 아이템"),
     MARKER("MK", "지도 마커"),
-    BORDER("BR", "프로필 테투리");
+    BORDER("BR", "프로필 테두리");
 
     private final String code;              //코드 값
     private final String description;       //한글 설명
 
-
-
+    StoreItemCategory(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
