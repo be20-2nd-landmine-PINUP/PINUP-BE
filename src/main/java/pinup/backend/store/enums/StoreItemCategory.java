d72b396f8f944e5cc45ca3dc6a123d45f941fdfd
sangdo19909-code <sangdo19909@gmail.com>
2025-10-30 17:11:30 +0900

    hotfix : store기능 Eunm 수정하였습니다.

--- src/main/java/pinup/backend/store/enums/StoreItemCategory.java
+++ src/main/java/pinup/backend/store/enums/StoreItemCategory.java
@@ -5,13 +5,7 @@ import lombok.Getter;
 @Getter
 public enum StoreItemCategory {
 
-    BACKGROUD("BG", "배경 아이템"),
-    MARKER("MK", "지도 마커"),
-    BORDER("BR", "프로필 테투리");
-
-    private final String code;              //코드 값
-    private final String description;       //한글 설명
-
-
-
+    BACKGROUD,
+    MARKER,
+    BORDER;
 }
