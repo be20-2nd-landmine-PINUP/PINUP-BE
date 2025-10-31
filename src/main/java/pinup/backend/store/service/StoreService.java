11a60d39f7a8311a1d4388ab85b92b3676700c6b
sangdo19909-code <sangdo19909@gmail.com>
2025-10-31 11:40:35 +0900

    feature : store 기능
    StoreService 부분 추가하였습니다.
    story 필드 부분 수정하였습니다.

--- src/main/java/pinup/backend/store/service/StoreService.java
+++ src/main/java/pinup/backend/store/service/StoreService.java
@@ -1,4 +1,33 @@
 package pinup.backend.store.service;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pinup.backend.store.entity.Store;
+import pinup.backend.store.repository.StoreRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+    /*
+     *  상점 관련 비즈니스 로직
+     * - 지역 한정 아이템 조회
+     */
+
+    // 판매 중인 아이템 조회(지역코드로 한정판 구분 가능)
+    public List<Store> getActiveItems() {
+        return storeRepository.findAllByIsActiveTrue();
+    }
+
+    // 단일 아이템 상세정보 조회
+    public Store getItemById(Integer itemId) {
+        return storeRepository.findById(itemId)
+                .orElseThrow(() -> new IllegalArgumentException
+                        ("해당 아이템이 존재하지 않습니다. ID: " + itemId));
+    }
+
 }
