6f56a84f715ad324eda112eac24dd3269381a4d0
penrose0214 <penrose0214@gmail.com>
2025-11-04 09:01:08 +0900

    refactor : removed unnecessary entities, and added ManyToOne relation to Region and Territory.

--- src/main/java/pinup/backend/conquer/command/domain/entity/Territory.java
+++ src/main/java/pinup/backend/conquer/command/domain/entity/Territory.java
@@ -10,26 +10,23 @@ import java.util.Date;
 
 @Entity
 @Table(name = "territory")
-@Getter                                                 // 나중에 controller, service 수정시 수정 예정
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Territory {
-    // 1. PK ID 선언
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)     // auto-increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "territory_id", nullable = false)
     private long territoryId;
 
-    // 2. FK 선언, 단방향
-    @ManyToOne(fetch = FetchType.LAZY)                  // ToDo: 일단은 성능 향상을 위해서 LAZY fetch로 선언
-    @JoinColumn(
-            name = "user_id",
-            nullable = false,
-            foreignKey = @ForeignKey(name = "user_id")
-    )
-    private Users userId;                               // ToDo: 브랜치 merge 후에 확인 요망
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users userId;
 
-    // 3. 기타 속성 (필드로 선언)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region;
+
     @Column(name = "capture_start_at")
     private Date captureStartAt;
 
