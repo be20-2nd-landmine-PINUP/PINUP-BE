ae8e3f85d22f94efdbc8aa49db37a6763f61289f
405supply <405supplychoi@gmail.com>
2025-11-05 17:51:00 +0900

    feature : 알림 기능을 위한 sse 연결 수립 및 데이터 송신 기능 추가

--- src/main/java/pinup/backend/notification/controller/NotificationController.java
+++ src/main/java/pinup/backend/notification/controller/NotificationController.java
@@ -1,5 +1,6 @@
 package pinup.backend.notification.controller;
 
+import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import pinup.backend.notification.service.NotificationService;
 
+import java.util.concurrent.Executors;
+
 @RestController
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
+@PermitAll
 public class NotificationController {
     private final NotificationService notificationService;
 
     @GetMapping(value = "/connect/{clientId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter connect(@PathVariable Integer clientId) {
+    public SseEmitter connect(@PathVariable Long clientId) {
         return notificationService.establishConnect(clientId);
     }
 }
