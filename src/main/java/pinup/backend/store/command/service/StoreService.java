8403f36c5b75d651d295909a61e4059bdd2fa5a1
sangdo19909-code <sangdo19909@gmail.com>
2025-11-07 18:30:49 +0900

    store: region 코드 사용하여 한정판 아이템 구현
    한정판 아이템 , 이벤트 아이템 구현 수정 필요

--- src/main/java/pinup/backend/store/command/service/StoreService.java
+++ src/main/java/pinup/backend/store/command/service/StoreService.java
@@ -3,10 +3,13 @@ package pinup.backend.store.command.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pinup.backend.conquer.command.domain.entity.Region;
+import pinup.backend.conquer.command.domain.repository.TerritoryRepository;
 import pinup.backend.member.command.domain.Users;
 import pinup.backend.point.command.domain.PointSourceType;
 import pinup.backend.store.command.domain.Inventory;
 import pinup.backend.store.command.domain.Store;
+import pinup.backend.store.command.domain.StoreLimitType;
 import pinup.backend.store.command.repository.StoreRepository;
 import pinup.backend.point.command.service.PointService;
 
@@ -18,6 +21,7 @@ public class StoreService {
     private final StoreRepository storeRepository;
     private final InventoryService inventoryService;
     private final PointService pointService;
+    private final TerritoryRepository territoryRepository;
 
     /**
      * ✅ 아이템 구매:
@@ -33,6 +37,11 @@ public class StoreService {
 
         // 2️⃣ 이미 보유 중인지 확인
         inventoryService.validateOwnedItem(user, store);
+
+        // 지역 한정 아이템 여부 확인
+        if (StoreLimitType.LIMITED.equals(store.getLimitType())) {
+            validateLimitedItemPurchase(user, store);
+        }
 
         // 3️⃣ 포인트 잔액 확인 (포인트 모듈에서 제공)
         int userPoint = pointService.getUserTotalPoint(user.getUserId());
@@ -55,4 +64,14 @@ public class StoreService {
         // 5️⃣ 인벤토리 등록
         return inventoryService.addToInventory(user, store);
     }
+
+    private void validateLimitedItemPurchase(Users user, Store store) {
+        Region region = store.getRegion();
+
+        boolean conquered = territoryRepository.existsByUserIdAndRegion(user, region);
+        if (!conquered) {
+            throw new IllegalStateException("해당 지역을 점령한 사용자만 구매할 수 있습니다.");
+        }
+    }
+
 }
